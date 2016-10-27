ca677573418e861cc75cffdfbc566511b75f938e
Jyoti <jyoti@cnergee.com>
2016-10-27 12:50:15 +0530

    MotionEvent

--- app/src/main/java/company/myapplication/MainActivity.java
+++ app/src/main/java/company/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package company.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
+    @Override
+    public boolean onGenericMotionEvent(MotionEvent event) {
+        return super.onGenericMotionEvent(event);
+    }
 }
